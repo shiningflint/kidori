3e7f87e5f3323bac9ae9610915ce2f182b756d76
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-10-12 18:20:08 +0900

    First commit, react ready

---
+++ app/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hello Kidori</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      {test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader?name=assets/[name].[ext]'},
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
+}
+
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config;
