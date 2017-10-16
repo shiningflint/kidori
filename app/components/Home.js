1d1ff673510620cca098225edb350ab5d03c791e
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-10-16 11:34:24 +0900

    set transitions on component mount

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var bannerURL = "https://images.unsplash.com/photo-1463320898484-cdee8141c787?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=4fb4c867279f3f242bad30c12e91bf9b";
+
 class HomeLogo extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.setActive = this.setActive.bind(this);
+    this.setImage = this.setImage.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(this.setImage, this.props.timeout)
+  }
+
+  setActive() {
+    this.setState({
+      active: true,
+    });
+  }
+
+  setImage() {
+    let bgImg = new Image();
+    bgImg.src = this.props.url;
+    bgImg.onload = this.setActive;
   }
 
   render() {
     return(
-      <div className="home-image-wrap">
-        <img src={require('../../assets/logo.svg')} className="home-image" />
+      <div className={"home-image-wrap"+(this.state.active ? ' active' : '' )}>
+        <img src={this.props.url} className="home-image" />
       </div>
     );
   }
@@ -17,12 +40,26 @@ class HomeLogo extends React.Component {
 
 class HomeTxt extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.setActive = this.setActive.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(this.setActive, this.props.timeout)
+  }
+
+  setActive() {
+    this.setState({
+      active: true,
+    });
   }
 
   render() {
     return(
-      <h1 className="home-txt">
+      <h1 className={"home-txt"+(this.state.active ? ' active' : '' )}>
         植物と暮らそう
         <br/>小さな自然と四季を見つめながら
         <br/>何気ない日常を 心地よく 豊かに
@@ -34,14 +71,36 @@ class HomeTxt extends React.Component {
 
 class HomeBanner extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      active: false,
+    };
+    this.setActive = this.setActive.bind(this);
+    this.setImage = this.setImage.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(this.setImage, this.props.timeout);
+  }
+
+  setActive() {
+    this.setState({
+      active: true,
+    });
+  }
+
+  setImage() {
+    let bgImg = new Image();
+    bgImg.src = this.props.url;
+    bgImg.onload = this.setActive;
   }
 
   render() {
     return(
       <div
-        className="home-banner-wrap"
-        style={{backgroundImage: "url(https://images.unsplash.com/photo-1463320898484-cdee8141c787?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=4fb4c867279f3f242bad30c12e91bf9b)"}}></div>
+        className={"home-banner-wrap"+(this.state.active ? ' active' : '' )}
+        style={{backgroundImage: "url("+this.props.url+")"}}></div>
     );
   }
 }
@@ -54,9 +113,9 @@ class Home extends React.Component {
   render() {
     return(
       <div className="home-wrap">
-        <HomeBanner/>
-        <HomeTxt/>
-        <HomeLogo/>
+        <HomeBanner timeout={700} url={bannerURL} />
+        <HomeTxt timeout={500} />
+        <HomeLogo timeout={300} url={require('../../assets/logo.svg')} />
       </div>
     );
   }
